b3bdee292dab846deeeec09f63f9163f985524c4
avkoehl <avkoehl@ucdavis.edu>
2019-10-11 15:10:38 -0700

    plot rotated

--- js/get_topic_terms.js
+++ js/get_topic_terms.js
@@ -1,4 +1,4 @@
-NTERMS = 100;
+NTERMS = 30;
 
 function get_topic_terms() {
     // get the topic id from input value
@@ -13,43 +13,59 @@ function get_topic_terms() {
     xmlHttp.send("topicid=" + topicid)
 
     xmlHttp.onreadystatechange = function ()  {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-	   parse_topic_terms(JSON.parse(xmlHttp.responseText), topicid);
-        }// if success
+	if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+	    parse_topic_terms(JSON.parse(xmlHttp.responseText), topicid);
+	}// if success
     }//response recieved
 }//get_topic_terms
 
 function parse_topic_terms(data, topicid) {
-   // given the array of document objects File_ID Score
-   // sort object based on scores
-   // keep only the top NDOCS
-   var topic_terms = document.getElementById("topic_terms");
-   topic_terms.innerHTML = "";
+    // given the array of document objects File_ID Score
+    // sort object based on scores
+    // keep only the top NDOCS
+    var topic_terms = document.getElementById("topic_terms");
+    topic_terms.innerHTML = "";
 
-   var plot = document.createElement("div");
-   plot.id = "topic_terms_plot";
-   topic_terms.appendChild(plot);
-   data.sort(function(a,b) {
-       return ((a.Score > b.Score) ? -1 : ((a.Score == b.Score) ? 0 : 1));
-   });
+    var plot = document.createElement("div");
+    plot.id = "topic_terms_plot";
+    topic_terms.appendChild(plot);
+    data.sort(function(a,b) {
+	return ((a.Score > b.Score) ? -1 : ((a.Score == b.Score) ? 0 : 1));
+    });
 
-   data = data.slice(0,NTERMS);
+    data = data.slice(0,NTERMS);
 
-   var terms = [];
-   var scores = [];
-   for (var i = 0; i < data.length; i++)
-   {
-       terms.push(data[i]["Term"]);
-       scores.push(data[i]["Score"]);
-   }
+    var terms = [];
+    var scores = [];
+    for (var i = 0; i < data.length; i++)
+    {
+	terms.push(data[i]["Term"]);
+	scores.push(data[i]["Score"]);
+    }
 
-   var plot_data = [
-   {
-    x: terms,
-    y: scores,
-    type: 'bar'
-   }
-   ];
+    var plot_data = [
+	{
+	    x: scores,
+	    y: terms,
+	    type: 'bar',
+	    orientation: 'h'
+	}
+    ];
+    var layout = {
+	title: 'Topic Terms ' + topicid,
+	staticPlot: true,
+	autosize: "false",
+	height: 800,
+	width: 900,
+	yaxis: {autorange:"reversed"},
+	margin: {
+	    l: 50,
+	    r: 50,
+	    b: 50,
+	    t: 50,
+	    pad: 4
+	},
+    };
 
-   Plotly.newPlot('topic_terms_plot', plot_data, {title: 'Topic ' + topicid + ' Terms', showlegend: false, margin: { l:25, r:0, t:50, b:90, pad: 0}}, {staticPlot: true});
+    Plotly.newPlot('topic_terms_plot', plot_data, layout, {staticPlot: true});
 }
