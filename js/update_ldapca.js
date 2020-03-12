fe60ea44fcaceaa514550583a9db9bddca2a2a94
avkoehl <avkoehl@ucdavis.edu>
2020-03-12 14:39:37 -0700

    even sillier but works

--- js/update_ldapca.js
+++ js/update_ldapca.js
@@ -67,4 +67,33 @@ function update_ldapca(data) {
     document.getElementById("sinfo").innerHTML = JSON.stringify(sizes) ;
     Plotly.newPlot('ldapca', data, layout, {displayModeBar: false});
 
+    ldapca.on('plotly_click', function(data){
+	colors = JSON.parse(document.getElementById("cinfo").innerHTML);
+	sizes = JSON.parse(document.getElementById("sinfo").innerHTML);
+	gd = document.getElementById("ldapca");
+	var id = 0;
+
+	for(var i=0; i < data.points.length; i++){
+	    text = data.points[i].text;
+	    text = text.split("<br")[0];
+	    id = text.split(":")[1].trim();
+	}
+
+	document.getElementById("topic_terms").setAttribute("name", id);
+	get_topic_terms();
+	if (document.getElementById("lda_area").getAttribute("name") != "no_top_docs") {
+	    get_topic_documents();
+	}
+
+	// get colors and sizes from data
+	var pn = '', tn = '';
+	for (var i =0; i < data.points.length; i++) {
+	    pn = data.points[i].pointNumber;
+	    tn = data.points[i].curveNumber;
+	};
+	colors[pn] = '#a91111';
+	console.log("pn: "+ pn);
+	var update = {'marker':{color: colors, size:sizes, line: { color: 'black', width: 2}}};
+	Plotly.restyle('ldapca', update, [tn]);
+    });
 }
