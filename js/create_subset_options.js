83216ca851e2fc17571a9d46efcb9cf21f76cb51
Chandni <cjnagda@ucdavis.edu>
2020-08-10 11:31:26 -0700

    removed maxopts for lda subset #41

--- js/create_subset_options.js
+++ js/create_subset_options.js
@@ -1,34 +1,33 @@
 function create_subset_options(data) {
     //modified to use semantic ui selector not selectize!
-    MAXOPTS = 100;
 
     // keywords
     var kw = document.getElementById('selected-keywords')
-    for (var i = 0; i < MAXOPTS; i++) {
-	var val = data["keywords"][i];
-	var option = document.createElement("option");
-	option.text = val;
-	option.value = val;
-	kw.appendChild(option);
+    for (var i = 0; i < data["keywords"].length; i++) {
+    	var val = data["keywords"][i];
+    	var option = document.createElement("option");
+    	option.text = val;
+    	option.value = val;
+    	kw.appendChild(option);
     }
 
     // authors
     var a = document.getElementById('selected-authors')
-    for (var i = 0; i < MAXOPTS; i++) {
-	var val = data["authors"][i];
-	var option = document.createElement("option");
-	option.text = val;
-	option.value = val;
-	a.appendChild(option);
+    for (var i = 0; i < data["authors"].length; i++) {
+    	var val = data["authors"][i];
+    	var option = document.createElement("option");
+    	option.text = val;
+    	option.value = val;
+    	a.appendChild(option);
     }
 
     // locations
     var l = document.getElementById('selected-locations')
-    for (var i = 0; i < MAXOPTS; i++) {
-	var val = data["locations"][i];
-	var option = document.createElement("option");
-	option.text = val;
-	option.value = val;
-	l.appendChild(option);
+    for (var i = 0; i < data["locations"].length; i++) {
+    	var val = data["locations"][i];
+    	var option = document.createElement("option");
+    	option.text = val;
+    	option.value = val;
+    	l.appendChild(option);
     }
 }
