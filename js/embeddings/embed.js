56ddeaf51471b22ac6203ceec64abf34f01cc5d1
Chandni Nagda <chandninagda@gmail.com>
2020-08-31 10:39:05 -0700

    put back word change annotations

--- js/embeddings/embed.js
+++ js/embeddings/embed.js
@@ -1,3 +1,134 @@
+var timeseriesLayout = {
+    autosize: false,
+    width: 1024,
+    height: 550,
+    title: ' ',
+    xaxis: {
+        title: 'Decades',
+        gridcolor: 'rgb(243, 243, 243)',
+        type: 'linear',
+        range: [1470, 1705],
+        dtick: 10,
+        zerolinewidth: 1,
+        ticklen: 1,
+	tickfont: {color: 'gray'},
+        gridwidth: 1
+    },
+    yaxis: {
+        title: 'term Change',
+        gridcolor: 'rgb(243, 243, 243)',
+        layer: 'below traces',
+        range: [0, 1.05],
+        dtick: 0.25,
+        gridwidth: 1,
+        showticklabels: true,
+        tickmode: 'array',
+        tickvals: [.25, .50, .75, 1.0],
+        ticktext: ['25', '50', '75', '100%'],
+        tickfont: {color: 'gray'}
+    },
+    paper_bgcolor: 'rgb(243, 243, 243)',
+    plot_bgcolor: 'rgb(243, 243, 243)',
+    showlegend: false,
+    hovermode: 'closest',
+    displayModeBar: false
+};
+
+var startingLayout = {
+    autosize: false,
+    width: 1024,
+    height: 550,
+    title: ' ',
+    xaxis: {
+        title: 'Decades',
+        gridcolor: 'rgb(243, 243, 243)',
+        type: 'linear',
+        range: [1470, 1705],
+        dtick: 10,
+        zerolinewidth: 1,
+        ticklen: 1,
+	tickfont: {color: 'gray'},
+        gridwidth: 1
+    },
+    yaxis: {
+        title: 'term Change',
+        gridcolor: 'rgb(243, 243, 243)',
+        layer: 'below traces',
+        range: [0, 1.05],
+        dtick: 0.25,
+        gridwidth: 1,
+        showticklabels: true,
+    	tickmode: 'array',
+    	tickvals: [.25, .50, .75, 1.0],
+    	ticktext: ['25', '50', '75', '100%'],
+    	tickfont: {color: 'gray'}
+    },
+    annotations: [
+        {
+            x: 1700,
+            y: 1,
+            xref: 'Decades',
+            yref: 'term Change',
+            text: 'each decade is <br> compared with 1700',
+            showarrow: true,
+            arrowhead: 6,
+            ax: 0,
+            ay: -40
+        },
+        {
+            x: 1490,
+            y: 0.4503,
+            xref: 'Decades',
+            yref: 'term Change',
+            text: 'lowest point is most <br> different from 1700',
+            showarrow: true,
+            arrowhead: 6,
+            ax: 0,
+            ay: 40
+        },
+        {
+            x: 1650,
+            y: 0.4636,
+            xref: 'Decades',
+            yref: 'term Change',
+            text: 'terms nearest to <br> term in 1700',
+            showarrow: true,
+            arrowhead: 6,
+            ax: 20,
+            ay: 40
+        },
+        {
+            x: 1670,
+            y: 0.572,
+            xref: 'Decades',
+            yref: 'term Change',
+            text: '',
+            showarrow: true,
+            arrowhead: 6,
+            ax: -50,
+            ay: 56
+        },
+        {
+            x: 1505,
+            y: 0.67,
+            xref: 'Decades',
+            yref: 'term Change',
+            text: 'abrupt increase indicates <br> term meaning change',
+            showarrow: true,
+            arrowhead: 2,
+            ax: -90,
+            ay: -85
+        }
+    ],
+    //margin: { pad: 20 },
+    paper_bgcolor: 'rgb(243, 243, 243)',
+    plot_bgcolor: 'rgb(243, 243, 243)',
+    showlegend: false,
+    hovermode: 'closest',
+    displayModeBar: false
+};
+
+
 /* helper function which returns an array of ints given a range */
 const range = (start, stop, step = 1) =>
 Array(Math.ceil((stop - start) / step)).fill(start).map((x, y) => x + y * step);
@@ -27,7 +158,7 @@ $(document).ready(function() {
         var term = 'history';
         var neighborsTimeseries = data.timeseries.slice(1, data.timeseries.length);
         plotWordChange(term, decades, data.timeseries[0],
-            data.decades, neighborsTimeseries, timeseriesLayout);
+            data.decades, neighborsTimeseries, startingLayout);
         plotHist('full', null, data.full, term);
         $("#dropdown-auth").change(function () {
            var author = $(this).val();
