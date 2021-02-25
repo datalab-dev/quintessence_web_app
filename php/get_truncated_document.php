cb26531302eb0186a380555ea69043e97957077c
Arthur Koehl <avkoehl@ucdavis.edu>
2021-02-25 09:46:46 -0800

    split getting documents into two php scripts

--- php/get_truncated_document.php
+++ php/get_truncated_document.php
@@ -1,39 +1,35 @@
 <?php
 /**
-get_document.php
+get_truncated_document.php
 
-Given document id and type, return full document text.
+Given document id, return truncated version of the doucment text.
 
 args (either GET parameters or command line):
-    qid  -- integer (0 - ~60k) 
-    type -- string (lemma, std, pos, text)
+    qid -- integer (0 - ~60k)
 
 authors:    Arthur Koehl, Chandni Nagda
- */ 
+ */
 
 require 'vendor/autoload.php';
 require 'config.php';
 
 if ($_GET) {
     $qid = (int)$_GET['qid'];
-    $type = $_GET['type'];
 } else {
     $qid = (int)$argv[1];
-    $type = $argv[2];
 }
 
-$collectionName = 'docs' . '.' . $type;
 $db = getMongoCon();
-$collection = $db->{$collectionName};
+$collection = $db->{'docs.truncated'};
 $cursor = $collection->find(
     [
-	'_id' => $qid
+        '_id' => $qid
     ],
     [
-	'projection' => [
-	    '_id' => 0,
-	    $type => 1
-	]
+        'projection' => [
+            '_id' => 0,
+            'text' => 1
+        ]
     ]
 );
 
