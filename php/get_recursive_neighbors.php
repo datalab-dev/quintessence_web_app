d3a185c37fdb48e4f75f01fb84ff58d4efe5b078
Arthur Koehl <avkoehl@ucdavis.edu>
2021-07-01 23:37:17 -0700

    need to filter in javascript + convert to gephi format

--- php/get_recursive_neighbors.php
+++ php/get_recursive_neighbors.php
@@ -3,13 +3,13 @@
 get_recursive_neighbors.php
 
 Given term, and model return nearest topn neighbors 
-for each neighbor, also return its topn neighbors that are in the 
-neighbors list of the query term 
+for each neighbor get it's distance with the other neighbors
 
 args (either GET parameters or command line):
-    term  -- string
-    model -- string
-    topn  --  int
+    term      -- string
+    modeltype -- string
+    modelname -- string
+    topn      --  int
 
 authors:    Arthur Koehl
  */
@@ -17,23 +17,55 @@ authors:    Arthur Koehl
 require 'vendor/autoload.php';
 require 'config.php';
 
+function get_neighbors($term, $modelname, $modeltype, $db, $topn) {
+    $collection = $db->{'terms.neighbors'};
+    $res = $collection->findOne(
+	[
+	    '_id' => $term
+	]
+    );
+
+    $terms = array();
+    $scores = array();
+
+    # Get list of neighbors
+    if ($modeltype == "full") {
+	$terms = array_slice($res[$modeltype]["terms"], 0, $topn);
+	$terms = array_slice($res[$modeltype]["scores"], 0, $topn);
+    }
+    else {
+	$terms = array_slice($res[$modeltype][$modelname]["terms"], 0, $topn);
+	$scores = array_slice($res[$modeltype][$modelname]["scores"], 0, $topn);
+    }
+
+
+    return(
+	array("terms" => $terms, "scores" => $scores)
+    );
+}
+
+$db = getMongoCon();
+
 if ($_GET) {
     $term = $_GET['term'];
-    $model = $_GET['model'];
+    $modeltype = $_GET['modeltype'];
+    $modelname = $_GET['modelname'];
     $topn = $_GET['topn'];
 } else {
     $term = $argv[1];
-    $model = $argv[2];
+    $modeltype = $argv[2];
+    $modelname = $argv[3];
     $topn = 10;
 }
-$db = getMongoCon();
 
-$collection = $db->{'terms.neighbors'};
-$res = $collection->findOne(
-    [
-	'_id' => $term:
+$all_neighbors = array();
+$neighbors = get_neighbors($term, $modelname, $modeltype, $db, $topn);
+$all_neighbors[$term] = $neighbors;
 
-    ]
-);
-echo json_encode($res);
+# foreach neighbor get similarity to the other neighbors
+foreach ($neighbors["terms"] as $n) {
+    $all_neighbors[$n] = get_neighbors($n, $modelname, $modeltype, $db, $topn);
+}
+
+echo json_encode($all_neighbors);
 ?>
